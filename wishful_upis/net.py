70a2076acc07d980a8ddcc3f29e536979fc69890
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-02-10 16:57:08 +0100

    init

---
+++ wishful_upis/net.py
@@ -0,0 +1,18 @@
+__author__ = "Piotr Gawlowicz, Mikolaj Chwalisz"
+__copyright__ = "Copyright (c) 2015, Technische Universitat Berlin"
+__version__ = "0.1.0"
+__email__ = "{gawlowicz, chwalisz}@tkn.tu-berlin.de"
+
+
+def start_iperf_server():
+    '''
+        func desc
+    '''
+    pass
+
+
+def stop_iperf_server():
+    '''
+        func desc
+    '''
+    pass
---
+++ wishful_upis/radio.py
@@ -0,0 +1,26 @@
+__author__ = "Piotr Gawlowicz, Mikolaj Chwalisz"
+__copyright__ = "Copyright (c) 2015, Technische Universitat Berlin"
+__version__ = "0.1.0"
+__email__ = "{gawlowicz, chwalisz}@tkn.tu-berlin.de"
+
+
+def set_channel(channel):
+    '''
+    set_channel 
+    ch - channel to set
+    '''
+    pass
+
+
+def get_channel():
+    '''
+        func desc
+    '''
+    pass
+
+
+def set_power(power):
+    '''
+        func desc
+    '''
+    pass
