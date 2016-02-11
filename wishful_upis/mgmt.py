128557e606e4c755a393b5e53c1180db6b525c62
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-02-11 15:14:20 +0100

    update

--- wishful_upis/mgmt.py
+++ wishful_upis/mgmt.py
@@ -4,14 +4,14 @@ __version__ = "0.1.0"
 __email__ = "{gawlowicz, chwalisz}@tkn.tu-berlin.de"
 
 
-def start_local_control_loop():
+def start_local_control_loop(control_loop):
     '''
         func desc
     '''
     pass
 
 
-def stop_local_control_loop():
+def stop_local_control_loop(control_loop_id):
     '''
         func desc
     '''
