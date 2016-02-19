314b89e3fbe35da2c3b6d3aabd036961e2cbaf24
zubow <zubow@tkn.tu-berlin.de>
2016-02-19 18:52:36 +0100

    ho function added

--- wishful_upis/global_upi.py
+++ wishful_upis/global_upi.py
@@ -25,14 +25,16 @@ def transaction_commit():
     pass
 
 
-def perform_soft_handover(srcAp, dstAp, station):
+def perform_soft_handover(wifi_intf, wlan_inject_iface, sta_mac_addr, sta_ip_addr, servingAP, servingAP_ip_addr, servingAP_channel,
+                           servingAP_bssid, targetAP, targetAP_ip_addr, targetAP_channel, gateway):
     '''
         func desc
     '''
     pass
 
 
-def perform_hard_handover(srcAp, dstAp, station):
+def perform_hard_handover(wifi_intf, wlan_inject_iface, sta_mac_addr, sta_ip_addr, servingAP, servingAP_ip_addr, servingAP_channel,
+                           servingAP_bssid, targetAP, targetAP_ip_addr, targetAP_channel, gateway):
     '''
         func desc
     '''
