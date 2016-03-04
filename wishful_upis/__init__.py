da7cbbfe8935fd7d3f0a94c2fbc4b1c8515e8205
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-03-04 12:11:46 +0100

    added context upis

--- wishful_upis/__init__.py
+++ wishful_upis/__init__.py
@@ -2,3 +2,4 @@ from net import *
 from radio import *
 from mgmt import *
 from net_func import *
+from context import *
---
+++ wishful_upis/context.py
@@ -0,0 +1,11 @@
+__author__ = "Piotr Gawlowicz"
+__copyright__ = "Copyright (c) 2015, Technische Universitat Berlin"
+__version__ = "0.1.0"
+__email__ = "{gawlowicz}@tkn.tu-berlin.de"
+
+
+def get_position():
+    '''
+        func desc
+    '''
+    pass
