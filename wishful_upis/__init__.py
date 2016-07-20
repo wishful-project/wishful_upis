1769cbf063c66817fb31c159f0a02bd98a586a13
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-07-20 14:07:54 +0200

    UPI definitions in classes

--- wishful_upis/__init__.py
+++ wishful_upis/__init__.py
@@ -7,3 +7,15 @@ from . import context
 from . import lte
 from . import wifi
 from . import zigbee
+
+from .tools import copy_functions_to_module
+
+copy_functions_to_module(net)
+copy_functions_to_module(radio)
+copy_functions_to_module(mgmt)
+copy_functions_to_module(net_func)
+copy_functions_to_module(context)
+
+copy_functions_to_module(lte)
+copy_functions_to_module(wifi)
+copy_functions_to_module(zigbee)
