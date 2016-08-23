3414bdbbc1c35ae9927ff660880895f78046911c
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-08-23 18:02:02 +0200

    cleaning not needed events

--- wishful_upis/mgmt.py
+++ wishful_upis/mgmt.py
@@ -10,14 +10,14 @@ __email__ = "{gawlowicz, chwalisz, zubow}@tkn.tu-berlin.de"
     The WiSHFUL management interface.
 '''
 
-#TODO: few of therm are API, remove it from UPIs
+
 class Mgmt(Upi):
     ''' Wishful rule-matching engine '''
+
     def add_rule(self, rule):
         '''Add new rule to rule-matching engine
         '''
         pass
-
 
     def delete_rule(self, rule_id):
         '''Remove rule to rule-matching engine
@@ -27,38 +27,40 @@ class Mgmt(Upi):
     '''
         Framework functionality
     '''
+
     def start_local_control_program(self, program_name, program_code):
-        '''Execute a given control program on loca/remote node.
+        '''
+        Execute a given control program on loca/remote node.
         '''
         pass
-
 
     def stop_local_control_program(self, program_id):
-        '''Stops execution of a given control program on loca/remote node.
+        '''
+        Stops execution of a given control program
+        on loca/remote node.
         '''
         pass
 
-
     def send_msg_to_local_control_program(self, program_id, msg):
-        '''Hierarchical control function allows the global control program to send messages to local control programs.
+        '''
+        Hierarchical control function allows the global
+        control program to send messages to local control programs.
         '''
         pass
 
     def transaction_begin(self):
-        '''Start a transaction
-
+        '''
+        Start a transaction
         i.e. all subsequent UPI calls are executed in transactional scope.
         '''
         pass
 
-
     def transaction_abort(self):
-        '''Aborts a running transaction
-
+        '''
+        Aborts a running transaction
         i.e. all changes are roll backed.
         '''
         pass
-
 
     def transaction_commit(self):
         '''Commit an open transaction.
@@ -83,13 +85,6 @@ class ControllerDiscoveredEvent(EventBase):
         self.ulink = ulink
 
 
-class ControllerConnectionResponseEvent(EventBase):
-    def __init__(self, cmdDesc, msg):
-        super().__init__()
-        self.cmdDesc = cmdDesc
-        self.msg = msg
-
-
 class ControllerConnectedEvent(EventBase):
     def __init__(self, ctrUuid):
         super().__init__()
@@ -106,13 +101,6 @@ class ControllerLostEvent(EventBase):
     def __init__(self, ctrUuid):
         super().__init__()
         self.ctrUuid = ctrUuid
-
-
-class NewNodeDiscoveredEvent(EventBase):
-    def __init__(self, cmdDesc, msg):
-        super().__init__()
-        self.cmdDesc = cmdDesc
-        self.msg = msg
 
 
 class NewNodeEvent(EventBase):
@@ -142,49 +130,6 @@ class HelloMsgEvent(EventBase):
         super().__init__()
 
 
-class ConnectToControllerEvent(EventBase):
-    def __init__(self, dlink, ulink):
-        super().__init__()
-        self.dlink = dlink
-        self.ulink = ulink
-
-
-class DisconnectControllerEvent(EventBase):
-    def __init__(self):
-        super().__init__()
-
-
-class SubscribeTopicEvent(EventBase):
-    def __init__(self, topic):
-        super().__init__()
-        self.topic = topic
-
-
-class UnsubscribeTopicEvent(EventBase):
-    def __init__(self, topic):
-        super().__init__()
-        self.topic = topic
-
-
-class SendMgsEvent(EventBase):
-    def __init__(self, msg):
-        super().__init__()
-        self.msg = msg
-
-
-class SendControllMgsEvent(EventBase):
-    def __init__(self, msg):
-        super().__init__()
-        self.msg = msg
-
-
-class CommandEvent(EventBase):
-    def __init__(self, dest, cmdDesc, msg):
-        super().__init__()
-        self.dest = dest
-        self.cmdDesc = cmdDesc
-        self.msg = msg
-
 class ExceptionEvent(EventBase):
     def __init__(self, dest, cmdDesc, msg):
         super().__init__()
@@ -192,12 +137,6 @@ class ExceptionEvent(EventBase):
         self.cmdDesc = cmdDesc
         self.msg = msg
 
-class ReturnValueEvent(EventBase):
-    def __init__(self, dest, cmdDesc, msg):
-        super().__init__()
-        self.dest = dest
-        self.cmdDesc = cmdDesc
-        self.msg = msg
 
 class CtxCommandEvent(EventBase):
     def __init__(self, ctx):
@@ -205,11 +144,13 @@ class CtxCommandEvent(EventBase):
         self.ctx = ctx
         self.responseQueue = None
 
+
 class CtxReturnValueEvent(EventBase):
     def __init__(self, ctx, msg):
         super().__init__()
         self.ctx = ctx
         self.msg = msg
+
 
 class TimeEvent(EventBase):
     """docstring for TimeEvent"""
