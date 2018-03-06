36722a866b83d6dcfc545874aa7063587da31c73
pruckebusch <peter.ruckebusch@ugent.be>
2018-03-06 14:30:01 +0100

    adding extra upi functions for OTA reprogramming of sensor nodes

--- wishful_upis/mgmt.py
+++ wishful_upis/mgmt.py
@@ -64,3 +64,94 @@ def transaction_commit():
     '''Commit an open transaction.
     '''
     pass
+
+
+def disseminate_radio_program(radio_program_id, radio_program, nodes, source_node=0):
+    """This function allows to disseminate radio programs to one or more nodes.
+    Optionally, a source node can be selected that manages the dissemination process.
+
+    Args:
+        radio_program_id (int): Radio program identifier.
+        radio_program (file): Binary file containing radio program.
+        nodes (list[int]): List of node IDs that need to receive the update.
+        source_node (int, optional): ID of the node that manages the dissemination process.
+
+    Returns:
+        int: Error value 0 = SUCCESS; -1 = FAIL
+    """
+    return
+
+
+def inject_radio_program(radio_program_id, nodes, source_node=0):
+    """This function allows to inject a previously disseminated radio programs in one or more nodes.
+    Optionally, a source node can be selected that manages the injection process.
+
+    Args:
+        radio_program_id (int): Radio program identifier.
+        nodes (list[int]): List of node IDs that need to inject the radio program.
+        source_node (int, optional): ID of the node that manages the injection process.
+
+    Returns:
+        int: Error value 0 = SUCCESS; -1 = FAIL
+    """
+    return
+
+
+def allocate_memory(module_id, elf_file_size, rom_size, ram_size, nodes):
+    """This function allocates memory on one or more nodes.
+    The allocated memory will be used to store the software module and/or radio program.
+    This step is required when using an offline ELF linker because the exact memory location must be known upfront.
+
+    Args:
+        elf_file_size (int): Size of the ELF file that needs to be stored.
+        rom_size (int): ROM usage of the code contained in the ELF file.
+        ram_size (int): RAM usage of the code contained in the ELF file.
+        nodes (list[int]): List of node IDs that need to allocate_memory.
+
+    Returns:
+        AllocatedMemoryBlock: Returns the allocated ROM and RAM addressess, repeats the sizes.
+    """
+    return
+
+
+def disseminate_software_module(module_id, elf_object_file, nodes, source_node=0):
+    """This function allows disseminating a software module (i.e. ELF object file) to one or more nodes.
+
+    Args:
+        elf_object_file_id (int): ELF object file identifier
+        elf_object_file (file): ELF object file
+        nodes (list[int]): List of node IDs that need to receive the ELF file.
+        source_node (int, optional): ID of the node that manages the dissemination process.
+
+    Returns:
+        int: Error value 0 = SUCCESS; -1 = FAIL
+    """
+    return
+
+
+def install_software_module(module_id, nodes, source_node=0):
+    """This function allows installing a previously disseminated software module on one or more nodes.
+
+    Args:
+        elf_object_file_id (int): ELF object file identifier
+        nodes (list[int]): List of node IDs that need to install the ELF file.
+        source_node (int, optional): ID of the node that manages the installation process.
+
+    Returns:
+        int: Error value 0 = SUCCESS; -1 = FAIL
+    """
+    return
+
+
+def activate_software_module(module_id, nodes, source_node=0):
+    """This function allows activating a previously installed software module on one or more nodes.
+
+    Args:
+        elf_object_file_id (int): ELF object file identifier
+        nodes (list[int]): List of node IDs that need to activate the ELF file.
+        source_node (int, optional): ID of the node that manages the activation process.
+
+    Returns:
+        int: Error value 0 = SUCCESS; -1 = FAIL
+    """
+    return
